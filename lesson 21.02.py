f2bc3e776ba06cfd57ef4a9e5225431f65d4cb07
yevayak0112-del <yevayak0112@gmail.com>
2026-02-21 17:04:33 +0200

    lesoon 21.02

---
+++ lesson 21.02.py
@@ -0,0 +1,26 @@
+"""
+def назва_функції(аргументи):
+    код функції
+
+
+
+    назва_функції(значення)
+"""
+
+def first_function():
+    print('Hello students')
+
+first_function()
+print(first_function)
+
+def second_function():
+    hello = "Hello students"
+    name = input("What is your name?")
+    return hello, name
+
+print (second_function)
+print(second_function())
+#print(hello) ---> print("Hello students")
+
+
+
