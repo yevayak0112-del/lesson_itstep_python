88341584f8a08dfe583458eab249c94caa08fd84
yevayak0112-del <yevayak0112@gmail.com>
2026-02-21 17:37:03 +0200

    lesson 21.02.py 2

--- lesson 21.02.py
+++ lesson 21.02.py
@@ -5,7 +5,6 @@ def назва_функції(аргументи):
 
 
     назва_функції(значення)
-"""
 
 def first_function():
     print('Hello students')
@@ -21,6 +20,28 @@ def second_function():
 print (second_function)
 print(second_function())
 #print(hello) ---> print("Hello students")
+
+
+def hello(arg1, arg2):
+    return arg1 + arg2
+
+print(hello)
+print(hello("Hello", "World"))
+print(hello(3, 5))
+print(hello(input("arg_1-"), input("arg_2-")))
+x = "IT"
+y = " Step"
+print(hello(x, y))
+"""
+
+def s_triangle(a, h):
+    s = 0.5 * a * h
+    return s
+
+print(f"площа трикутника s = {s_triangle(5, 6)}")
+print(f"площа трикутника s ="
+      f"{s_triangle(int(input('a=')), int(input('h=')))}")
+
 
 
 
