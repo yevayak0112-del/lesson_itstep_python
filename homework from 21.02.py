ac2fe84586d24c35c2b383bc1a3b8de7cdb205e1
yevayak0112-del <yevayak0112@gmail.com>
2026-02-22 15:35:18 +0200

    homework from 21.02

---
+++ homework from 21.02.py
@@ -0,0 +1,19 @@
+def square(x):
+    return x ** 2
+print(square(5))
+
+def hello_function():
+    print('Hello world')
+hello_function()
+
+def sum_numbers(a, b):
+    return a + b
+print(sum_numbers(3, 7))
+
+def power(x, n=2):
+    return x ** n
+print(power(3))
+
+def rectangle_area(width, height):
+    return width * height
+print(rectangle_area(4, 5))
